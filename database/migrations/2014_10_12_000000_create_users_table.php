325ee71ac2b26f6af0c33e96a71b8ea289ce9c18
MBP <mbp@MBPs-MacBook-Pro.local>
2018-05-07 13:24:40 +0800

    init projects

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('avatar');
+            $table->string('phoneNumber')->unique();
+            $table->integer('follows_count')->default(0)->commit('关注数');
+            $table->integer('followers_count')->default(0)->commit('粉丝数');
+            $table->integer('favourites_count')->default(0)->commit('收藏数');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
